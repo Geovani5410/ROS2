046ce80815a6a44288fad3a11d0b2885c8d8d080
Geovani Said <geovaniajaja@gmail.com>
2025-06-30 08:59:25 -0600

    Delete github_ws directory

--- github_ws/build/github/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/geovani/github_ws/install/github'
--- github_ws/install/github/lib/python3.10/site-packages/github/git.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from github.')
-
-
-if __name__ == '__main__':
-    main()
